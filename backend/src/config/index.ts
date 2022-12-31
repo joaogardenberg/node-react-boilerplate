4b380f53ddfa44620f6092f9e3cbc75516da3f74
Johnny <joaogardenberg@gmail.com>
2022-12-31 17:06:23 -0300

    Create config files

---
+++ backend/src/config/index.ts
@@ -0,0 +1,21 @@
+interface Config {
+  [key: string]: string | undefined
+}
+
+export const FRONTEND_URL = process.env.FRONTEND_URL as string
+export const NODE_ENV = process.env.NODE_ENV as string
+export const PORT = process.env.PORT as string
+
+const config: Config = {
+  FRONTEND_URL,
+  NODE_ENV,
+  PORT
+}
+
+Object.keys(config).forEach((key: string) => {
+  if (!config[key]) {
+    console.warn(`[WARNING] There is no ${key} environment variable set!`)
+  }
+})
+
+export default config
---
+++ frontend/src/config/index.ts
@@ -0,0 +1,21 @@
+interface Config {
+  [key: string]: string | undefined
+}
+
+export const NODE_ENV = process.env.NODE_ENV as string
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL as string
+
+const config: Config = {
+  NODE_ENV,
+  BACKEND_URL
+}
+
+Object.keys(config).forEach((key: string) => {
+  if (!config[key]) {
+    console.warn(
+      `[WARNING] There is no REACT_APP_${key} environment variable set!`
+    )
+  }
+})
+
+export default config
