f13a639408ad15154af5a5936e8368294235d089
Johnny <joaogardenberg@gmail.com>
2023-01-01 14:16:49 -0300

    Use server

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Application } from 'express'
+import { Server } from 'http'
 import notFoundRoute from './notFound'
 
-export default function addRoutes(app: Application) {
+export default function addRoutes(app: Application, server: Server) {
+  server.on('connection', () => console.info('Someone connected!'))
   app.get('/', (_, res) => res.send('Boilerplate: Backend'))
 
   // Has to be last
