cb2a1fc478edf5b80a81e02ffebf10fcbd2269ae
Johnny <joaogardenberg@gmail.com>
2024-11-15 05:29:22 +0100

    Move config to right place

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { PORT } from './config'
+import { PORT } from './src/config'
 
 // https://vite.dev/config/
 export default defineConfig({
