45ea1a81fafc91fb4d42ba83afd729e73f3fee20
Johnny <joaogardenberg@gmail.com>
2024-11-15 05:54:03 +0100

    Refactor

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -2,17 +2,15 @@ interface Config {
   [key: string]: string | undefined
 }
 
-export const NODE_ENV = process.env.NODE_ENV!
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL!
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 const config: Config = {
-  NODE_ENV,
   BACKEND_URL
 }
 
 Object.keys(config).forEach((key: string) => {
   if (!config[key]) {
-    console.warn(`[WARNING] There is no REACT_APP_${key} environment variable set!`)
+    console.warn(`[WARNING] There is no VITE_${key} environment variable set!`)
   }
 })
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: parseInt(process.env.PORT!) },
-  resolve: { alias: { src: '/src' } }
+  server: { port: parseInt(process.env.PORT) },
+  resolve: {
+    alias: {
+      components: '/src/components',
+      config: '/src/config'
+    }
+  }
 })
