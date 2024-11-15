5fbd0dec539f68ea4e7e791022935ef92bbd98a2
Johnny <joaogardenberg@gmail.com>
2024-11-15 06:14:51 +0100

    Refactor config

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,15 +1,27 @@
+import { isNil } from 'lodash'
+
 interface Config {
-  [key: string]: string | undefined
+  [key: string]: unknown
 }
 
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL as string
+export const BASE_URL = import.meta.env.BASE_URL
+export const DEV = import.meta.env.DEV
+export const MODE = import.meta.env.MODE
+export const PROD = import.meta.env.PROD
+export const SSR = import.meta.env.SSR
 
 const config: Config = {
-  BACKEND_URL
+  BACKEND_URL,
+  BASE_URL,
+  DEV,
+  MODE,
+  PROD,
+  SSR
 }
 
 Object.keys(config).forEach((key: string) => {
-  if (!config[key]) {
+  if (isNil(config[key])) {
     console.warn(`[WARNING] There is no VITE_${key} environment variable set!`)
   }
 })
