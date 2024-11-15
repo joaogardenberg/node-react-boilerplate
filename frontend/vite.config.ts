7b37fe8fd825f2236050911e75309a6fc0878e92
Johnny <joaogardenberg@gmail.com>
2024-11-15 05:58:50 +0100

    Fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: parseInt(process.env.PORT) },
+  server: { port: parseInt(process.env.PORT!) },
   resolve: {
     alias: {
       components: '/src/components',
