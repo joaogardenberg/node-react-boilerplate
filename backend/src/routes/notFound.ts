aec420575751f4a454d98e57f70c9a3f6707faef
Johnny <joaogardenberg@gmail.com>
2025-04-20 23:41:20 +0200

    Fix route name

--- backend/src/routes/notFound.ts
+++ backend/src/routes/notFound.ts
@@ -2,5 +2,5 @@ import { Application } from 'express'
 import { FRONTEND_URL } from 'config'
 
 export default function notFoundRoutes(app: Application) {
-  app.get('*', ({ originalUrl }, res) => res.redirect(`${FRONTEND_URL}${originalUrl}`))
+  app.get('*all', ({ originalUrl }, res) => res.redirect(`${FRONTEND_URL}${originalUrl}`))
 }
