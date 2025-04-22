222ed5d838031e9332f3543e7997f348857d2d9a
Johnny <joaogardenberg@gmail.com>
2025-04-22 14:24:10 +0200

    Test

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,13 @@
 import { Application } from 'express'
 import { Server } from 'http'
 import notFoundRoute from './notFound'
+import config from 'config'
 
 export default function addRoutes(app: Application, server: Server) {
   server.on('connection', () => console.info('Someone connected!'))
 
   app.get('/', (_, res) => {
-    res.send('Sporthood: Backend')
+    res.send(config)
   })
 
   // Has to be last
